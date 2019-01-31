8cafdfbff7621150932dd4eb3d94ddbdbc764060
Chase Hilbert <chase@heyspacetime.com>
2019-01-31 06:08:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,14 @@ def prime?(num)
   numbers = 0..num
   puts numbers.to_a
 
-  return false if num <= 1 
+  return false if num <= 1
 
 
 numbers.each do |integer|
-  if num % integer != 0
+  if integer <= 1
+    return false
+  elsif num % integer != 0
     return true
-  break
     else
     return false
 end
