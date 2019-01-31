f54a55ed67ae6bdc0eb9fa11ae13681522b9e740
Chase Hilbert <chase@heyspacetime.com>
2019-01-31 05:59:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,13 @@ def prime?(num)
   numbers = 2...num
   puts numbers.to_a
 
+
 numbers.each do |integer|
   if num % integer == 0
     return false
+    break
+    else
     integer += 1
-
 end
 end
 end
